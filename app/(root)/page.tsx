3ddaaad1d9b2c282b296e3694cdc7b59e60497fb
Keyukemi <keyukemi.ku@gmail.com>
2023-07-05 20:25:44 +0100

    more modal work

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,15 @@
+'use client'
+import { Modal } from "@/components/ui/modal";
 import { UserButton } from "@clerk/nextjs";
 
 const SetupPage = () => {
     return (
       <div>
-       
         <UserButton afterSignOutUrl="/"/>
-        Hello Admin Dashboard
+        <Modal isOpen onClose={()=>{}} title="Test" description="Testing 123">
+            children
+        </Modal>
+        
       </div>
     )
   }
